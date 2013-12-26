f669dd5f6b46ab2c6d86a06a2a6c1891563072bb
Barrett17 <b.vitruvio@gmail.com>
2013-12-26 16:21:00 +0100

    Updated MediaRecorder.

--- src/media/MediaRecorder.h
+++ src/media/MediaRecorder.h
@@ -1,47 +1,51 @@
 
 class MediaRecorder {
 public:
-						MediaRecorder(const char* name,
-							int32 priority = 0);
+									MediaRecorder(const char* name,
+										int32 priority = 0);
 
-	virtual					~MediaRecorder();
+	virtual							~MediaRecorder();
 
-		status_t			InitCheck();
+			status_t				InitCheck() const;
 
-		const media_node &		Node();
+			const media_node&		Node() const;
 
-		status_t			SetHook(void* cookie,
-							void (*record_hook)(
-							void* cookie,
-							void* data,
-							size_t size,
-							const media_header& header));
+			status_t				SetHook(void* cookie,
+										void (*record_hook)(
+										void* cookie,
+										void* data,
+										size_t size,
+										const media_header& header));
 
-		status_t			Connect(const dormant_node_info& info,
-							const media_format* format = 0,
-							uint32 flags = 0);
+			status_t				Connect(const dormant_node_info& info,
+										const media_format* format = 0,
+										uint32 flags = 0);
 
-		status_t			Connect(const media_node& node,
-							const media_output* useOutput = 0,
-							const media_format* format = 0,
-							uint32 flags = 0);
+			status_t				Connect(const media_node& node,
+										const media_output* useOutput = 0,
+										const media_format* format = 0,
+										uint32 flags = 0);
 
-		status_t			Disconnect();
-		status_t			Start();
-		status_t			Stop();
+			bool					IsConnected() const;
 
-		bool				IsRunning();
-		bool				IsStarted();
-		bool				IsConnected();
+			status_t				Disconnect();
 
-		const media_output &		Input();
-		const media_input&		Output();
+			status_t				Start();
+			status_t				Stop();
 
-		const media_format&		Format();
+			bool					IsStarted() const;
+
+			const media_output &	Input() const;
+			const media_input&		Output() const;
+
+			const media_format&		Format() const;
 
 protected:
-	virtual void				BufferReceived(void* data, size_t size,
-							const media_header& header);
+	virtual void					BufferReceived(void* data,
+										size_t size,
+										const media_header& header);
 private:
 
+			bool					fStarted;
+			
 };
