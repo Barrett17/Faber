254affde287f55d2f4e1418a788174822ecc80a3
Barrett <b.vitruvio@gmail.com>
2013-12-31 02:28:19 +0100

    Work on the block API.

--- src/media/MediaBlock.h
+++ src/media/MediaBlock.h
@@ -1,10 +1,28 @@
+/*
+    Copyright 2013 Dario Casalinuovo. All rights reserved.
+
+    This file is part of Faber.
+
+    Faber is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Faber is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Faber.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 
 class MediaBlock
 {
 
 			bool			IsEmpty() const;
-			void			MakeEmpty();
+			void			CleanUp();
 
 			status_t		ReadFrames(void* buffer, int64* frameCount);
 			int64			CountFrames() const;
@@ -24,6 +42,8 @@ class MediaBlock
 
 			status_t		DeleteFile();
 
+			bool			IsReady() const;
+
 private:
 			int64			fSize;
 
@@ -34,6 +54,8 @@ private:
 
 			BMediaFile*		fFile;
 			BMediaTrack*	fTrack;
+
+			bool			fReady;
 };
 
 
