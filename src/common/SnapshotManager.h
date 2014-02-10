f81d939d07898235fc671571651b2ad7ef2318a7
Barrett17 <b.vitruvio@gmail.com>
2014-02-10 01:15:53 +0000

    Some plans for SnapshotManager.

---
+++ src/common/SnapshotManager.h
@@ -0,0 +1,35 @@
+/*
+    Copyright 2013 Dario Casalinuovo. All rights reserved.
+
+    This file is part of Faber.
+
+    Faber is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Faber is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Faber.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+class SnapshotManager {
+public:
+									SnapshotManager();
+	virtual							~SnapshotManager();
+
+	uint32							GetNextRevision(uint32 rev);
+
+	uint32							CurrentRevision();
+
+	status_t						RestoreStatus(uint32 rev);
+
+	status_t						FreezeRequested(uint32* newRev) const;
+
+private:
+
+};
