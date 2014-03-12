29d7ac0e6072153899643e14898f5551c9c41784
Barrett17 <b.vitruvio@gmail.com>
2014-03-12 17:06:10 +0000

    Addedd AudioProtocolDefs.h

---
+++ src/audio/AudioProtocolDefs.h
@@ -0,0 +1,32 @@
+/*
+    Copyright 2013 Dario Casalinuovo. All rights reserved.
+
+    This file is part of Faber.
+
+    Faber is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Faber is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Faber.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef AUDIO_PROTOCOL_DEFS_H
+#define AUDIO_PROTOCOL_DEFS_H
+
+
+#define SAVE_TRACK_CHANNEL_NAME 			"track_channel"
+const uint32 SAVE_TRACK_CHANNEL				=	'stch';
+
+#define SAVE_TRACK_INDEX_NAME 				"track_index"
+const uint32 SAVE_TRACK_INDEX				=	'trix';
+
+#define SAVE_AUDIOTRACK_INDEX_NAME 			"audiotrack_index"
+const uint32 SAVE_AUDIOTRACK_INDEX			=	'atrx';
+
+#endif // AUDIO_PROTOCOL_DEFS_H
